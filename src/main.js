42a5a05a5989dbcf37fb0034eda2b6dd36c9955b
Karol Kozakowski <karol.kozakowski@artificial.io>
2020-06-01 20:29:37 +0200

    Use env variables from cognito

--- src/main.js
+++ src/main.js
@@ -1,33 +1,23 @@
 import App from './App.svelte';
 import Amplify from 'aws-amplify';
-import config from './aws';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: 'world'
+  }
 });
 
 window.app = app;
 
 Amplify.configure({
-	Auth: {
-		mandatorySignIn: true,
-		region: config.cognito.REGION,
-		userPoolId: config.cognito.USER_POOL_ID,
-		identityPoolId: config.cognito.IDENTITY_POOL_ID,
-		userPoolWebClientId: config.cognito.APP_CLIENT_ID
-	},
-	API: {
-		endpoints: [
-			{
-				name: "meals",
-				endpoint: config.apiGateway.URL,
-				region: config.apiGateway.REGION
-			},
-		]
-	}
+  Auth: {
+    mandatorySignIn: true,
+    region: process.env.REGION,
+    userPoolId: process.env.USER_POOL_ID,
+    identityPoolId: process.env.IDENTITY_POOL_ID,
+    userPoolWebClientId: process.env.APP_CLIENT_ID
+  }
 });
 
 
